7261d026863fafa281a58a4d0ebcf5b0dc087721
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-06-01 14:09:52 -0700

    mejora

--- Movies_CRUD/movies.c
+++ Movies_CRUD/movies.c
@@ -6,7 +6,7 @@
 /*-------- STRUCT MULTIMEDIA --------*/
 typedef struct
 {
-    char clave[10];
+    int clave;
     char titulo[30];
     int categoria;
     int tipo;
@@ -18,19 +18,22 @@ void insertar(Multim movies[], int *cont);
 void borrar(Multim movies[], int *cont);
 void modificar(Multim movies[], int *cont);
 void listar(Multim movies[], int *cont);
-void buscar(Multim movies[], int *cont);
-void asignarCategTipo(Multim movies[], char categ[], char tipo[], int indice);
+void buscar(Multim movies[], int cont);
 
 // Funciones auxiliares
+void imprimirRegistro(Multim movie);
 void leerCadena(const char msg[], char cadena[], int largo);
 int leerInt(const char msg[], int ri, int rf);
+void asignarCategTipo(Multim movies, char categ[], char tipo[]);
+void burbuMejorada(Multim vect[], int n);
+int busqSecuencial(Multim vect[], int m, int num);
 
 int main()
 {
     Multim movies[N];
-    int cont = 0;
+    int cont = 0, op;
+    int ordenado = 0;
 
-    int op;
     do
     {
         system("cls");
@@ -39,6 +42,7 @@ int main()
         {
         case 1:
             insertar(movies, &cont);
+            ordenado = 0;
             break;
         case 2:
             borrar(movies, &cont);
@@ -50,7 +54,19 @@ int main()
             listar(movies, &cont);
             break;
         case 5:
-            buscar(movies, &cont);
+            buscar(movies, cont);
+            break;
+        case 6:
+            if (!ordenado)
+            {
+                burbuMejorada(movies, cont);
+                printf("\n* Se ha ordenado por campo 'clave'\n");
+                ordenado = 1;
+            }
+            else
+            {
+                printf("* Error, ya se encuentra ordenado. *\n");
+            }
             break;
         case 0:
             printf("> Has salido <\n");
@@ -79,8 +95,9 @@ int menu()
     printf("3. Modificar\n");
     printf("4. Listar\n");
     printf("5. Buscar\n");
+    printf("6. Ordenar\n");
     printf("0. Salir\n");
-    op = leerInt("> Escoge una opcion: ", 0, 5);
+    op = leerInt("> Escoge una opcion: ", 0, 6);
     system("cls");
     return op;
 }
@@ -93,169 +110,162 @@ void insertar(Multim movies[], int *cont)
     printf("\t[ Registrando pelicula / serie #%d ]\n\n", *cont + 1);
 
     // Entrada de clave no repetida
+    int clave;
     do
     {
-        leerCadena("> Ingresa clave: ", movies[*cont].clave, 10);
+        clave = leerInt("> Ingresa clave [1000, 2000]: ", 1000, 2000);
+    } while (busqSecuencial(movies, *cont, clave) != -1);
 
-        for (int i = 0; i < *cont; i++) // Verifica si la clave no existe ya
-        {
-            if (strcmp(movies[i].clave, movies[*cont].clave) == 0)
-            {
-                printf("* La clave ya existe, ingrese nuevamente *\n");
-                existe = 1;
-            }
-            else
-            {
-                existe = 0;
-            }
-        }
-    } while (existe == 1);
+    movies[*cont].clave = clave; // Asigno clave valida
 
     // Entrada de titulo
     leerCadena("> Ingresa titulo: ", movies[*cont].titulo, 30);
 
     // Entrada de categoria
     printf("\n\t* Categoria *\n");
-    printf("1.-Accion\n2.-Terror\n3.-Thriller\n");
+    printf("[ 1.-Accion / 2.-Terror /3.-Thriller ]\n");
     movies[*cont].categoria = leerInt("> Ingresa categoria: ", 1, 3);
 
     // Entrada de tipo
     printf("\n\t* Tipo *\n");
-    printf("1.-CD\n2.-DVD\n3.-Blueray\n");
+    printf("[ 1.-CD / 2.-DVD /3.-Blueray ]\n");
     movies[*cont].tipo = leerInt("> Ingresa tipo: ", 1, 3);
 
     // Mensaje
     system("cls");
     printf("* Elemento agregado exitosamente. *\n\n");
+    imprimirRegistro(movies[*cont]);
     (*cont)++; // Incrementar contador
 }
 
 // Borra un elemento mediante su indice
 void borrar(Multim movies[], int *cont)
 {
-    int num;    // numero elemento a eliminar
-    int indice; // definir indice
+    int clave = leerInt("> Clave de elemento a eliminar [1000, 2000]: ", 1000, 2000);
+    int pos = busqSecuencial(movies, *cont, clave);
 
-    listar(movies, cont); // Mostrar elementos disponibles a eliminar
-
-    num = leerInt("\n> No. de elemento a eliminar: ", 0, *cont);
-
-    indice = num - 1;
-
-    if (indice < 0 || indice >= *cont)
+    if (pos != -1)
     {
-        printf("> Elemento no existente\n");
-        return;
+        imprimirRegistro(movies[pos]);
+        if (leerInt("> Desea eliminar ? [1. Si / 2. No]: ", 1, 2) == 1)
+        {
+            printf("* Elemento eliminado. *\n");
+            // Mueve un indice atras los elementos posteriores al eliminado
+            for (int i = pos; i < *cont; i++)
+            {
+                movies[i].clave = movies[i + 1].clave;
+                strcpy(movies[i].titulo, movies[i + 1].titulo);
+                movies[i].categoria = movies[i + 1].categoria;
+                movies[i].tipo = movies[i + 1].tipo;
+            }
+            (*cont)--; // Decrementar contador
+        }
+        else
+        {
+            printf("* Eliminacion cancelada. *\n");
+        }
     }
-
-    // Elimina el elemento en el indice ingresado
-    printf("\n> Elemento eliminado: %s : %s\n\n", movies[indice].clave, movies[indice].titulo);
-
-    // Mueve un indice atras los elementos posteriores al eliminado
-    for (int i = indice; i < *cont; i++)
+    else
     {
-        strcpy(movies[i].clave, movies[i + 1].clave);
-        strcpy(movies[i].titulo, movies[i + 1].titulo);
-        movies[i].categoria = movies[i + 1].categoria;
-        movies[i].tipo = movies[i + 1].tipo;
+        printf("* Elemento no encontrado. *\n");
     }
-    (*cont)--; // Decrementar contador
 }
 
 // Modifica elementos de la estructura
 void modificar(Multim movies[], int *cont)
 {
-    int indice, num, op;
+    int clave, op;
 
     listar(movies, cont);
-    num = leerInt("\n> Ingresa No. de elemento a modificar: ", 0, *cont);
-    indice = num - 1; // Convertir numero ingresado a indice para iterar arreglo
+    clave = leerInt("> Clave de elemento a modificar [1000, 2000]: ", 1000, 2000);
 
-    if (indice < 0 || indice >= *cont)
+    int pos = busqSecuencial(movies, *cont, clave);
+
+    if (pos != -1)
     {
-        printf("> Elemento no existente\n");
-        return;
+        // Pedir datos a modificar
+        printf("\nQue parametro deseas modificar: \n");
+        printf("1. Clave\n");
+        printf("2. Titulo\n");
+        printf("3. Categoria\n");
+        printf("4. Tipo\n");
+        op = leerInt("> ", 1, 4);
+
+        switch (op)
+        {
+        case 1:
+            movies[pos].clave = leerInt("Ingresa nueva clave [1000, 2000]: ", 1000, 2000);
+            break;
+        case 2:
+            leerCadena("Ingresa nuevo titulo: ", movies[pos].titulo, 30);
+            break;
+        case 3:
+            printf("\n\t* Categorias *\n");
+            printf("[ 1.-Accion / 2.-Terror /3.-Thriller ]\n");
+            movies[pos].categoria = leerInt("> ", 1, 3);
+            break;
+        case 4:
+            printf("\n\t* Tipos *\n");
+            printf("[ 1.-CD / 2.-DVD /3.-Blueray ]\n");
+            movies[pos].tipo = leerInt("> ", 1, 3);
+            break;
+        default:
+            printf("Ingrese una opcion valida.\n");
+            break;
+        }
+        printf("\n> Elemento modificado exitosamente.\n");
     }
-
-    // Pedir datos a modificar
-    printf("\nQue parametro deseas modificar: \n");
-    printf("1. Clave\n");
-    printf("2. Titulo\n");
-    printf("3. Categoria\n");
-    printf("4. Tipo\n");
-    op = leerInt("> ", 1, 4);
-
-    switch (op)
+    else
     {
-    case 1:
-        leerCadena("Ingresa nueva clave: ", movies[indice].clave, 10);
-        break;
-    case 2:
-        leerCadena("Ingresa nuevo titulo: ", movies[indice].titulo, 30);
-        break;
-    case 3:
-        printf("*Categorias*\n1.-Accion\n2.-Terror\n3.-Thriller\n");
-        movies[indice].categoria = leerInt("> ", 1, 3);
-        break;
-    case 4:
-        printf("*Tipos*\n1.-CD\n2.-DVD\n3.-Blueray\n");
-        movies[indice].tipo = leerInt("> ", 1, 3);
-        break;
-    default:
-        printf("Ingrese una opcion valida.\n");
-        break;
+        printf("* Elemento no encontrado. *\n");
     }
-    printf("\n> Elemento modificado exitosamente.\n");
 }
 
 // Muestra todos los elementos existentes
 void listar(Multim movies[], int *cont)
 {
     char categ[11] = "", tipo[11] = ""; // Variables temporales para nombre de categoria y tipo
-    printf("----------------------------------------------------------------------------------\n");
+    printf("+--------------------------------------------------------------------------------+\n");
+    printf("|                                CONTENIDO MULTIMEDIA                            |\n");
+    printf("|--------------------------------------------------------------------------------|\n");
     printf("|  No. |    Clave    |            Titulo            |   Categoria   |    Tipo    |\n");
-    printf("----------------------------------------------------------------------------------\n");
+    printf("|--------------------------------------------------------------------------------|\n");
 
     for (int i = 0; i < *cont; i++)
     {
-        asignarCategTipo(movies, categ, tipo, i); // Asignar leyenda de categoria y tipo
-
-        printf("| %4d | %-11s | %-28s | %-13s | %-10s |\n", i + 1, movies[i].clave, movies[i].titulo, categ, tipo);
+        asignarCategTipo(movies[i], categ, tipo); // Asignar leyenda de categoria y tipo
+        printf("| %4d | %-11d | %-28s | %-13s | %-10s |\n", i + 1, movies[i].clave, movies[i].titulo, categ, tipo);
     }
-    printf("----------------------------------------------------------------------------------\n");
+    printf("+--------------------------------------------------------------------------------+\n");
 }
 
 // Busca un elemento especifico por su clave
-void buscar(Multim movies[], int *cont)
+void buscar(Multim movies[], int cont)
 {
-    char clave[10];                     // Variable para leer la clave a buscar
-    char categ[11] = "", tipo[11] = ""; // Variables temporales para mostrar nombre de cat y tipo
+    int clave;                          // Variable para leer la clave a buscar
+    char categ[11] = "", tipo[11] = ""; // Variables temporales para mostrar nombre de categ y tipo
 
-    leerCadena("> Ingresa clave de elemento a buscar: ", clave, 10);
+    clave = leerInt("> Ingresa clave de elemento a buscar [1000, 2000]: ", 1000, 2000);
 
-    for (int i = 0; i < *cont; i++)
+    int pos = busqSecuencial(movies, cont, clave);
+
+    if (pos != -1) // Mostrar datos de busqueda
     {
-        if (strcmp(movies[i].clave, clave) == 0)
-        {
-            asignarCategTipo(movies, categ, tipo, i);
-            printf("\n> Elemento encontrado. \n[ Datos ]\n\n");
-            printf("* Posicion: %d\n", i + 1);
-            printf("* Clave: %s\n", clave);
-            printf("* Titulo: %s\n", movies[i].titulo);
-            printf("* Categoria: %s\n", categ);
-            printf("* Tipo: %s\n", tipo);
-            return; // Sale de la funcion al encontrar el elemento
-        }
+        asignarCategTipo(movies[pos], categ, tipo); // Asignar leyenda de categoria y tipo
+        printf("\n> Elemento encontrado. Posicion %d\n", pos);
+        imprimirRegistro(movies[pos]);
     }
-    // Si termina el ciclo, no existe el elemento
-    printf("> Elemento no existente.\n\n");
+    else
+    {
+        printf("> Elemento no existente.\n\n");
+    }
 }
 
 // Funcion para pasar de int a char 'categoria y tipo'
-void asignarCategTipo(Multim movies[], char categ[], char tipo[], int indice)
+void asignarCategTipo(Multim movies, char categ[], char tipo[])
 {
     // Definir nombre de 'Categoria'
-    switch (movies[indice].categoria)
+    switch (movies.categoria)
     {
     case 1:
         strcpy(categ, "Accion");
@@ -272,7 +282,7 @@ void asignarCategTipo(Multim movies[], char categ[], char tipo[], int indice)
     }
 
     // Definir nombre de 'tipo'
-    switch (movies[indice].tipo)
+    switch (movies.tipo)
     {
     case 1:
         strcpy(tipo, "CD");
@@ -287,6 +297,20 @@ void asignarCategTipo(Multim movies[], char categ[], char tipo[], int indice)
         strcpy(tipo, "Indefinido");
         break;
     }
+}
+
+void imprimirRegistro(Multim movie)
+{
+    char categ[20], tipo[20];
+    asignarCategTipo(movie, categ, tipo);
+    printf("+----------------------------------+\n");
+    printf("|            MULTIMEDIA            |\n");
+    printf("|----------------------------------|\n");
+    printf("| Clave : %-4d                     |\n", movie.clave);
+    printf("| Titulo : %-23s |\n", movie.titulo);
+    printf("| Categoria : %-15s      |\n", categ);
+    printf("| Tipo : %-10s                |\n", tipo);
+    printf("+----------------------------------+\n");
 }
 
 // Presentar un mensaje y Leer cadena con fgets
@@ -314,4 +338,39 @@ int leerInt(const char msg[], int ri, int rf)
         }
     } while (num < ri || num > rf);
     return num;
+}
+
+// Ordena arreglo struct por burbuja mejorada
+void burbuMejorada(Multim vect[], int n)
+{
+    int cambio = 1;
+    Multim aux;
+
+    for (int i = 0; i < n - 1 && cambio; i++)
+    {
+        cambio = 0; // Reinicia bandera
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (vect[j].clave > vect[j + 1].clave)
+            {
+                aux = vect[j];
+                vect[j] = vect[j + 1];
+                vect[j + 1] = aux;
+                cambio = 1; // Se realizo un cambio
+            }
+        }
+    }
+}
+
+// Busqueda secuencial struct (arreglo desordenado)
+int busqSecuencial(Multim vect[], int m, int num)
+{
+    for (int i = 0; i < m; i++)
+    {
+        if (vect[i].clave == num)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
